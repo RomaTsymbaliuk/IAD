3a0224d84c1294eb1572f7143b77af8b6f99b145
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-06-02 16:23:00 +0300

    Calculated correlation matrix.

--- lab1/main.py
+++ lab1/main.py
@@ -9,13 +9,21 @@ def initialize_variables():
     return X,Y
 
 def normalize_X(X):
-    X_normalized = X.copy(deep=True)
-    for column in X_normalized:
+    X_norm = X.copy(deep=True)
+    for column in X_norm:
         mean = X[column].mean()
         std = X[column].std()
-        for i, x in enumerate(X_normalized[column]):
-            X_normalized[column][i] = (x - mean) / std
-    return X_normalized
+        for i, x in enumerate(X_norm[column]):
+            X_norm[column][i] = (x - mean) / std
+    return X_norm.to_numpy()
+
+def correlation_matrix_calculate(X_norm):
+    N =  X_norm.shape[1]
+    X_transp = np.divide(X_norm.transpose(), N)
+    X_result = np.dot(X_transp, X_norm)
+    return X_result
+
 X,Y = initialize_variables()
 X_normalized = normalize_X(X)
+X_normalized = correlation_matrix_calculate(X_normalized)
 print(X_normalized)
