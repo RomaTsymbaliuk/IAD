a67c883323461175dc787e6161b1cfdc55d70e17
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-06-02 15:01:31 +0300

    Initial commit 2

--- lab1/main.py
+++ lab1/main.py
@@ -48,7 +48,5 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharmXXXX')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
->>>>>>> e183f56 (Initial commit)
