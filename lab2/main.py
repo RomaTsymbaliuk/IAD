36a9b8769b620d416cef689feaa4c85514fa882f
Roma-x <56560165+Roma-x@users.noreply.github.com>
2023-06-03 18:54:27 +0300

    [Lab2] Realized KMeans. Created plot for elbow method.

--- lab2/main.py
+++ lab2/main.py
@@ -10,10 +10,18 @@ dataframe.drop(columns=dataframe.columns[:2], axis=1, inplace=True)
 inertias = []
 
 def kmeans_clustering(data):
-    kmeans = KMeans(n_clusters=2)
-    model = kmeans.fit(data)
-    print(model.labels_)
+    inertias = []
 
+    for i in range(1, data.shape[0]):
+        kmeans = KMeans(n_clusters=i)
+        model = kmeans.fit(data)
+        inertias.append(model.inertia_)
+
+    plt.plot(range(1, data.shape[0]), inertias, marker='o')
+    plt.title('Elbow method')
+    plt.xlabel('Number of clusters')
+    plt.ylabel('Inertia')
+    plt.show()
 def prepare_data(data):
     prepared_data = [data.iloc[:, 0].to_numpy()[1:]]
     for i in range(1, data.shape[1]):
